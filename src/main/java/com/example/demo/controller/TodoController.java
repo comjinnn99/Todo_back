24aea75f0030da85163f845aa4afee6fd6de80f3
jhin <com.jinnn99@gmail.com>
2023-04-05 12:50:50 +0900

    CRUD #4

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -70,6 +72,25 @@ public class TodoController {
 			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
 					.data(dtos).build();
+			
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e){
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+					.error(error).build();
+			return ResponseEntity.ok().body(response);
+		}
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody TodoDTO dto){
+		try {
+			List<String> message = new ArrayList<>();
+			String msg = service.delete(dto.getId());
+			message.add(msg);
+			
+			ResponseDTO<String> response = ResponseDTO.<String>builder()
+					.data(message).build();
 			
 			return ResponseEntity.ok().body(response);
 		} catch (Exception e){
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -48,4 +48,14 @@ public class TodoService {
 		}
 		return repository.findById(entity.getId());
 	}
+	
+	public String delete(String id){
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		} else {
+			throw new RuntimeException("id does not exist");
+		}
+
+		return "deleted";
+	}
 }
